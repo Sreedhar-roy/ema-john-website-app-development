01c2bd1aedc70c93d22fd6ce539d3f259ef9f30d
Sreedhar-Roy <sreedharroy0@gmail.com>
2022-10-27 14:29:32 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Compornets/Header/Header';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import About from './Compornets/About/About';
 import Shop from './Compornets/Shop/Shop';
 
+
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path : '/',
+      element:<Main></Main>,
+      children:[
+        {
+          path: '/',
+          element: <Shop></Shop>,
+        },
+        {
+          path: 'about',
+          element: <About></About>,
+        },
+      ]
+    },
+    
+  ])
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Compornets/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>This is a secret about </h2>
+        </div>
+    );
+};
+
+export default About;
